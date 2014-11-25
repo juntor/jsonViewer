64c75609e29e3c774960f6a9bff83b4dcdb33a8d
shantj <tjbdsjs@gmail.com>
2014-11-25 14:56:22 +0800

    【bug】输入文字后焦点跳转的问题

--- JsonViewer/JsonViewer.cs
+++ JsonViewer/JsonViewer.cs
@@ -36,6 +36,13 @@ namespace EPocalipse.Json.Viewer
             }
         }
 
+        public void setMouse(int iIndex)
+        {
+            this.txtJson.Focus();//给richTextBox焦点
+            this.txtJson.Select(iIndex, 0);
+            this.txtJson.ScrollToCaret(); //滚动到控件光标处 
+        }
+
         [Editor("System.ComponentModel.Design.MultilineStringEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
         public string Json
         {
@@ -48,7 +55,9 @@ namespace EPocalipse.Json.Viewer
                 if (_json != value)
                 {
                     _json = value.Trim();
+                    int iIndex = txtJson.SelectionStart;
                     txtJson.Text = _json;
+                    setMouse(iIndex);
                     Redraw();
                 }
             }
